e3061b3c1a71d22261dbbd2a6baa35d99756acb8
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2023-09-03 14:34:55 -0300

    feat: finish clock and counter exercise

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,27 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+import Spinner from "../Spinner/Spinner.js";
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const savedValue = window.localStorage.getItem("saved-count");
+
+    if (savedValue === null) {
+      return;
+    }
+
+    setCount(savedValue);
+  }, [setCount]);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("saved-count", count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
-      Count: {count}
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
+      Count: {count !== null ? count : <Spinner />}
     </button>
   );
 }
